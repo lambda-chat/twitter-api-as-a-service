97b09159653f64ddde10b8fb2d8c0f9b4e15ed15
lambda-chat <chat.algebrique@gmail.com>
2022-05-08 16:27:46 +0900

    Initial commit

---
+++ test/twitter-api-as-a-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TwitterApiAsAService from '../lib/twitter-api-as-a-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/twitter-api-as-a-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TwitterApiAsAService.TwitterApiAsAServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
